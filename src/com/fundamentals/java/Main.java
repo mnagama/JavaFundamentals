d85cf0cdad53dbe63340382ab3e434124b93fcff
Roger O'Dell <odell.instructor@gmail.com>
2020-04-23 10:55:16 -0500

    Lesson 3 Exercise

--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
        System.out.println("Hello Java");
+       System.out.println("This is my first sentence");
+       System.out.println("I am new to Java");
+       System.out.println("This Java course is cool");
+       System.out.println("I am learning stuff every day");
     }
 }
