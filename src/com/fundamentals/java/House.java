aec807be03aa05231b2dbca605ba8003517343af
Roger O'Dell <odell.instructor@gmail.com>
2020-04-23 13:41:30 -0500

    Lesson 4 - My Application Overview

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,15 @@
+package com.fundamentals.java;
+
+/* This class is part of Lesson 4
+* My Application Overview */
+public class House {
+    String foundation;
+    String roofType;
+    String doorColor;
+    int windowSize;
+
+    public void doorOpenClose() {
+        System.out.println("The door is open");
+    } // end method doorOpenClose
+
+} // end class House
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -1,5 +1,7 @@
 package com.fundamentals.java;
 
+/* This class was created when the project
+* was created */
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +11,12 @@ public class Main {
        System.out.println("I am new to Java");
        System.out.println("This Java course is cool");
        System.out.println("I am learning stuff every day");
-    }
-}
+
+       myHouseExample();
+    } // End main method
+
+    public static void myHouseExample() {
+        House myHouse = new House();
+        myHouse.doorOpenClose();
+    } // End myHouseExample
+} // end class Main
