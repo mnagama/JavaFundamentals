2576b94c8fb79e935ca526975ad40b758d04561f
Roger O'Dell <odell.instructor@gmail.com>
2020-04-29 15:22:17 -0500

    Lesson 5 Primitive Data Types

--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -5,15 +5,26 @@ package com.fundamentals.java;
 public class Main {
 
     public static void main(String[] args) {
+        lesson5Examples();
 	// write your code here
-       System.out.println("Hello Java");
-       System.out.println("This is my first sentence");
-       System.out.println("I am new to Java");
-       System.out.println("This Java course is cool");
-       System.out.println("I am learning stuff every day");
+       //System.out.println("Hello Java");
+       //System.out.println("This is my first sentence");
+       //System.out.println("I am new to Java");
+       //System.out.println("This Java course is cool");
+       //System.out.println("I am learning stuff every day");
 
-       myHouseExample();
+       //myHouseExample();
     } // End main method
+
+    public static void lesson5Examples() {
+        Lesson5 myFive = new Lesson5();
+        myFive.showLocalExample();
+        int example = myFive.showLocalWithReturn(5,7); // return to a variable
+        System.out.println(example); // print variable from line above
+        System.out.println(myFive.showLocalWithReturn(2,9)); // direct return of int
+        myFive.showInstanceVariable();
+        myFive.showConversions();
+    } // end method lesson5Examples
 
     public static void myHouseExample() {
         House myHouse = new House();
